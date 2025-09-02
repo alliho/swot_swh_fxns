7f4782816cc2881d51eef769c5861eeb872f67cd
Alli <allison.j.ho@jpl.nasa.gov>
2025-09-02 16:46:56 -0700

    Adding Figure 6

---
+++ fig_code/fig_structurefunction_hs.m
@@ -0,0 +1,209 @@
+base_path = '/Users/ajho/Documents/JPL/';
+local_path = [base_path 'papers/swot_swh_calval/swot_swh_fxns/'];
+addpath([local_path 'swh_fxns/matlab/'])
+swot_fpath = [base_path '/data/SWOT/onedayrepeat/'];
+
+%%% ADD DATA
+load('/Users/ajho/Documents/myrepos/supportingdata/coastline_labeled.mat');
+load('/Users/ajho/Documents/myrepos/supportingdata/nicejet.mat');
+
+%%% OTHER DEPENDENCIES:
+% m_map
+% gitsio/code_universal/*/
+    % isin.m
+    % plot_mcoast.m
+    % load_any_nc.m
+    % setfigsize.m
+    % drawbox.m
+    % cleanLegend.m
+    % quietaxes.m
+    % textbypos.m
+    % setaxes.m
+    % format_fig4print.m
+    % subsetcmap.m
+
+% tight_subplot.m
+% AddLetters2Plots.m
+
+
+%% load
+
+clear data
+fpath = [local_path '/data/'];
+tmp = load([fpath 'NDBC_x_SWOT_science.mat']); data(1) = tmp.data;
+tmp = load([fpath 'GNSS_x_SWOT_calval.mat']); data(2) = tmp.data;
+tmp = load([fpath 'NDBC_x_SWOT_calval.mat']); data(3) = tmp.data;
+data(1).obstype = 'buoys';
+data(2).obstype = 'moorings';
+data(3).obstype = 'buoys';
+
+%% define stats
+
+
+getnn = @(x,y) find(~isnan(x) & ~isnan(y));
+getnnn = @(x,y,z) find(~isnan(x) & ~isnan(y) & ~isnan(z));
+biasfxn = @(x,y) nanmean((y-x));
+rmsefxn = @(x,y) sqrt(nanmean((x-y).^2));
+crmsefxn = @(x,y) sqrt(rmsefxn(x,y).^2 - biasfxn(x,y).^2);
+normrmsefxn = @(x,y) sqrt(nanmean((x-y).^2))./nanmean(abs(x));
+absrmsefxn = @(x,y) sqrt(nanmean((abs(x)-abs(y)).^2));
+nfxn = @(x,y) sum(~isnan(x) & ~isnan(y));
+sifxn = @(x,y) 1./nanmean(x(getnn(x,y))) .* sqrt( 1./length(x(getnn(x,y))) .* nansum( ( (y(getnn(x,y))-nanmean(y(getnn(x,y)))) - (x(getnn(x,y))-nanmean(x(getnn(x,y))))   ).^2 ) );;
+linfxn = @(x,y) polyfit(x(getnn(x,y)), y(getnn(x,y)), 1);
+getind = @(var,i) var(i); 
+intcptfxn = @(x,y) getind(linfxn(x,y),2); 
+slpfxn = @(x,y) getind(linfxn(x,y),1); 
+slp0fxn = @(x,y) y(getnn(x,y))/x(getnn(x,y));
+
+ccfxn = @(x,y) mode(diag(flipud(corrcoef(x(getnn(x,y)),y(getnn(x,y))))));
+
+
+
+
+%% [save] one plot for moorings, structure function in units meters
+dd = 2;
+bins = [1 5:10:100 125:25:200 300:100:1000]; 
+nmin = 2;
+
+
+hslims = [1 10];
+clear sts
+mipairs = nchoosek(1:length(data(dd).id),2);
+distpairs = NaN(length(mipairs), size(data(dd).t,2));
+for mi=1:length(mipairs) 
+    mi1 = mipairs(mi,1); mi2 = mipairs(mi,2);
+    % dist = distance(data(dd).lat0(mi1), data(dd).lon0(mi1), data(dd).lat0(mi2), data(dd).lon0(mi2), referenceEllipsoid('wgs84'))./1000;
+    dist = distance(data(dd).lat(mi1,:), data(dd).lon(mi1,:), data(dd).lat(mi2,:), data(dd).lon(mi2,:), referenceEllipsoid('wgs84'))./1000;
+    distpairs(mi,:) = dist;
+end
+
+ 
+qualflag = 1;
+uncflag = 0;
+corropt = 1;
+
+% -------------------------------------------------------------------------
+figure(69954); clf; 
+setfigsize(gcf, [483   375])
+% tight_subplot(Nh, Nw, [gap_h gap_w], [lower upper], [left right]) 
+% oldha = tight_subplot(2, 4, [0.025 0.05], [0.11 0.05], [0.05 0.05]);
+% [ha] = reorg_tightsubplot(oldha, {[1 2],[4 5]+1, [3 7], [4 8
+ha = tight_subplot(1, 1, [0.025 0.015], [0.12 0.025], [0.12 0.03]);
+% [ha] = reorg_tightsubplot(oldha, {[1 2],[4 5], [3 6]});
+
+% -------------------------------------------------------------------------
+setaxes(ha(:),'YLim', [0 1].*0.35);
+setaxes(ha(:),'CLim', [0 70]);
+% setaxes(ha(:),'Colormap', subsetcmap(nicejet, 20));
+linkaxes(ha(:))
+
+format_fig4print(ha)
+setaxes(ha(:),'XLabel', 'separation distance $r$ [km]');
+setaxes(ha(:),'YLabel', '$|SWH(x) - SWH(x+r)|$ [m]');
+% quietaxes(ha(2:end), 'y');
+
+setloglog(ha)
+setaxes(ha(:),'XDir', 'reverse');
+if dd == 1; xlims = [6 1100]; ylims = [0.000005 2]*0.1; else; xlims = [6 130]; ylims = [0.000005 0.23]*30; end;
+ylims = [0.002 0.8]
+setaxes(ha(:),'XLim', xlims);
+setaxes(ha(:),'YLim', ylims);
+setaxes(ha(:),'XTick', [10 20 50 100  200 500]);
+setaxes(ha(:), 'YMinorGrid', 'off')
+
+setaxes(ha(:),'CLim', [0 max(distpairs)]);
+setaxes(ha(:),'CLim', [0.5 5]);
+
+setaxes(ha(:),'YTick', [0.01 0.05 0.1 0.2]);
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+fldnms = { 'swh_karin_qc', 'swh_nadir_altimeter', 'swh_model'};
+fldquas = { 'swh_karin_qual_avg', 'swh_karin_qual_avg', 'swh_karin_qual_avg'};
+fldlbs = { 'SWOT KaRIn', 'SWOT nadir', 'model', 'SWOT KaRIn'};
+
+fldnms = {  'swh_model', 'swh_karin_qc',};
+fldquas = { 'swh_karin_qual_avg', 'swh_karin_qual_avg'};
+fldlbs = {  'model', 'SWOT KaRIn',};
+
+
+fldnms = {  'swh_model', 'swh_karin_proc_patch'}; 
+% fldnms = {  'swh_model', 'swh_karin_proc'}; 
+% fldnms = {  'swh_model', 'swh_karin_proc_unsmoothed'}; 
+% fldnms = {  'swh_model','swh_karin_proc_patch', 'swh_karin_proc_unsmoothed'}; 
+fldlbs = {  'model', 'SWOT KaRIn','SWOT KaRIn'};
+
+cmap = subsetcmap(nicejet, length(fldnms)+4); cmap = flipud(cmap([end-4 end],:)); 
+cmap(2,:) = cmap(2,:) + [-0.2 -0.15 0.15];
+cmap = flipud(cmap);
+cmap(cmap>1) = 1;
+cmap = [cmap; cmap(2,:)]
+
+for hi=1:length(fldnms)
+    fldnm = fldnms{hi};
+    ylb = fldlbs{hi};
+    % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    t_swot = data(dd).t(1,:); 
+    var_mo = data(dd).hs';
+    var_swot = data(dd).(fldnm)';
+    xtrck_swot = data(dd).cross_track_distance'; xtrck_swot = abs(xtrck_swot);
+    NN = isnan(data(dd).swh_karin_uncorr' + data(dd).swh_model' + data(dd).swh_nadir_altimeter'); 
+    NN = NN | squeeze(sum(cell2mat(cellfun(@(fldnm) permute(isnan(data(dd).(fldnm)), [1 3 2]), fldnms, 'Un', 0)),2))';
+    var_mo(NN) = NaN; var_swot(NN) = NaN; 
+    if corropt & contains(fldnm, 'karin') & ~contains(fldnm, 'proc')
+       corr = mdl(var_swot, xtrck_swot);
+       var_swot = var_swot - corr; 
+    end
+
+    xdata = NaN(length(t_swot), length(mipairs));
+    ydata = NaN(length(t_swot), length(mipairs));
+    zdata = NaN(length(t_swot), length(mipairs));
+
+
+    for mi=1:length(mipairs)
+        mi1 = mipairs(mi,1); mi2 = mipairs(mi,2);
+        % dist = distpairs(mi);
+        xdata(:,mi) = var_mo(:,mi1) - var_mo(:,mi2);
+        ydata(:,mi) = var_swot(:,mi1) - var_swot(:,mi2);
+        var0 = nanmean([var_mo(:,mi1) var_mo(:,mi2)]');
+        zdata(:,mi) = var0;
+    end
+
+    bdata = distpairs';
+    xdata = xdata(:); ydata = ydata(:); zdata = zdata(:); bdata = bdata(:);
+
+
+    % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    thisha = ha(1); axes(thisha); hold on; 
+    ii = find(~isin(zdata, hslims)); xdata(ii) = NaN; ydata(ii) = NaN;
+    nn = find(~isnan(xdata) & ~isnan(ydata)); xdata = xdata(nn); ydata = ydata(nn); zdata = zdata(nn); bdata = bdata(nn); 
+    
+    bindata = bdata; 
+    n = 1; 
+    if hi==1
+        var = abs(xdata).^n;  var = abs(var); col = [0.8 0.1 0.2];  col = [0 0 0]+0.2;
+        sts = boxplot_bybin(gca, var, bindata, bins, 'nmin',5, 'color', col, 'FaceAlpha',0.6, 'wid', 0.75.*mode(diff(bins)/2), 'plot', 0);
+        nn = find(sts.n>=nmin);
+        % shaded_limits(sts.x0(nn), sts.q2(nn), [sts.p10(nn)'; sts.p90(nn)' ], col, 'LineWidth',2, 'FaceAlpha', 0.05)
+        shaded_limits(sts.x0(nn), sts.q2(nn), [sts.q1(nn)'; sts.q3(nn)' ], col, 'LineWidth',2, 'FaceAlpha', 0.1)
+        scatter(sts.x0(nn), sts.q2(nn), 40, 'k', 'filled', 's', 'MarkerFaceColor', col, 'LineWidth',0.5, 'MarkerEdgeColor', 'k' , 'Displayname', 'GNSS buoy $H_s$')
+        plot(sts.x0(nn), sts.q3(nn), 'k:', 'Color', col)
+        plot(sts.x0(nn), sts.q1(nn), 'k:', 'Color', col)
+    end
+
+    var = abs(ydata).^n; var = abs(var); col = [0.2 0.1 0.8]; col = cmap(hi,:);
+    sts = boxplot_bybin(gca, var, bindata, bins, 'nmin',5, 'color', col, 'FaceAlpha',0.6, 'wid', 0.75.*mode(diff(bins)/2), 'plot', 0);
+    nn = find(sts.n>=nmin);
+    % shaded_limits(sts.x0(nn), sts.q2(nn), [sts.p10(nn)'; sts.p90(nn)' ], col, 'LineWidth',2, 'FaceAlpha', 0.1)
+    shaded_limits(sts.x0(nn), sts.q2(nn), [sts.q1(nn)'; sts.q3(nn)' ], col, 'LineWidth',2, 'FaceAlpha', 0.2)
+    scatter(sts.x0(nn), sts.q2(nn), 40, 'k', 'filled', 's', 'MarkerFaceColor', col, 'LineWidth',0.5, 'MarkerEdgeColor', 'k' , 'Displayname', [ylb ' SWH'])
+    plot(sts.x0(nn), sts.q3(nn), 'k:', 'Color', col)
+    plot(sts.x0(nn), sts.q1(nn), 'k:', 'Color', col)
+
+end
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+thisha = ha(1); axes(thisha); hold on; 
+cleanLegend(gca, 'northeast', 'NumColumns',3, 'Interpreter', 'latex')
+
+
+
